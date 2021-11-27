81b4799682a78cbb5bbf59bed9e148e8bf08ebba
Bo Henriksen <bomarconi@protonmail.com>
2021-11-27 16:30:20 +0100

    changed express to use ES6 modules

--- README.md
+++ README.md
@@ -13,9 +13,13 @@ yarn start
 http://localhost:3000/
 ``` 
 
-## ES6
+## ES6 Modules
 
-"type": "module", in package.json
+Since Node version 14.x ES modules are officially supported and stable <https://nodejs.org/docs/latest-v14.x/api/esm.html#esm_modules_ecmascript_modules> - So it's possible to use ES6's import instead of require.
+
+This repository has been setup to use ES6 modules.
+
+possible to "type": "module", in package.json
 
 ## Apollo Server
 
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from'morgan';
+import { fileURLToPath } from 'url';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
 
 var app = express();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -38,4 +41,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
